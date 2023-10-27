26394e7ba6b5e677fd04e440ff7a4cf60ef72999
luisdomain88 <147002646+luisdomain88@users.noreply.github.com>
2023-10-27 11:37:18 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="es">
 <head>
@@ -31,6 +32,7 @@
             }
         }
     </script>
+
 <script type="text/javascript">
         function getVariableFromURl(name) {
             name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
@@ -619,10 +621,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
        
 
         <span class="support" style="font-size:22px;">Póngase en contacto con el soporte técnico:&nbsp;
-        <!--<img src="phone1.png"style="width: 200px;" id="banner">-->
-	<br />
-        <span style="border:1px solid #114d9a;border-radius:5px;padding:2px 5px"> <script>document.write(phone_number)</script></span> 
-       <br />(Línea gratuita)
+        <img src="phone1.png"style="width: 200px;" id="banner">
+        <!--<span style="border:1px solid #114d9a;border-radius:5px;padding:2px 5px"> <script>document.write(phone_number)</script> -->
+       
+(Línea gratuita)
 <br /><span style="color: green">SOPORTE 100% GRATUITO</span>
 </span>
       </span>
